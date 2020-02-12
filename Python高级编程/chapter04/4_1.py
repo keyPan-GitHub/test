75798874bd9f6eabf3f56eb9de33d15746332189
KeyPan <13651220389@163.com>
2020-02-12 17:06:03 +0800

    高级编程练习

---
+++ Python高级编程/chapter04/4_1.py
@@ -0,0 +1,21 @@
+class Cat(object):
+    def say(self):
+        print("i am a cat")
+
+
+class Dog(object):
+    def say(self):
+        print("i am a dog")
+
+
+class Duck(object):
+    def say(self):
+        print("i am a duck")
+
+
+animal_list = [Cat, Dog, Duck]
+
+for animal in animal_list:
+    animal().say()
+
+
