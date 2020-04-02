553683c20de8a7c687d8f08060edbf8f770a246b
KeyPan <13651220389@163.com>
2020-04-02 19:47:44 +0800

    重新开始

--- python自动化/表格自动化/写xlwt.py
+++
@@ -1,9 +0,0 @@
-import xlwt
-
-#写
-
-new_workbook = xlwt.Workbook()
-workSheet = new_workbook.add_sheet('new_sheet')
-workSheet.write(0, 0, 'test')
-
-new_workbook.save('d:/test.xlsx')
--- python自动化/邮件自动化/1-4.py
+++
@@ -1,8 +0,0 @@
-import zmail
-
-server = zmail.server('gongzuo7853@163.com', 'pxy1010995752')
-mail = server.get_latest()
-# zmail.show(mail)
-print(mail['subject'])
-
-zmail.save_attachment(mail, target_path=None, overwrite=True)
