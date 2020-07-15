968b53dd50d6239e32358c3e0da27f43dc9d7dfc
keyPan-GitHub <13651220389@163.com>
2020-07-16 00:13:13 +0800

    列表 ，字典

---
+++ domes.py
@@ -0,0 +1,12 @@
+proson = {}
+proson['first'] = '黑龙江'
+proson['last'] = '辽宁'
+
+susan = {'first':'北京', 'last':'上海'}
+
+print(proson)
+print(susan)
+
+people = [proson,susan]
+people.append({'first':'浙江', 'last':'福建'})
+print(people)
