cf637b275073f125b7d6427f4f580d57cfc7edec
keyPan-GitHub <13651220389@163.com>
2020-08-08 13:32:31 +0800

    第二天

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+from collections import defaultdict
+from typing import DefaultDict
+import click
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return "hello world"
+@app.route('/index',defaults={'name':'Programmer'})
+@app.route('/index/<name>')
+def index(name):
+    return "<h1>hello %s!</h1>" % name
 
+@app.cli.command()
+def hello():
+    click.echo('Hello, Human!')
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
